2942a92f2754ff3097277450a1d7b1ad6c4ea546
TanishDadheech <tdadheech07@gmail.com>
2025-09-03 01:00:32 +0530

    Secured

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,27 @@
 import mongoose,{ model, Schema } from "mongoose";
 
-mongoose.connect("mongodb+srv://tdadheech07:tanish%4007102002@cluster0.2dndh.mongodb.net/Alt-Brain");
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
 const UserSchema = new Schema({
     username: {type:String, unique:true},
     password: String
 })
+
+const ContentSchema = new Schema({
+    
+    link: String,
+    type: String,
+    title: String,
+    username: {type:String, unique:true},
+})
+
+
+
 
 export const UserModel= model( "Users", UserSchema);
